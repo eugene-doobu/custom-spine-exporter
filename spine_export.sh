b451eac5ee7f5254e1514f47f70edc83381b3f23
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-02-26 14:52:16 +0900

    Update spine_export.sh

--- spine_export.sh
+++ spine_export.sh
@@ -35,7 +35,7 @@ fi
 
 # if no export settings file is presented use json+pack by default
 if [ ! -f "$EXPORT_SETTINGS" ]; then
-	EXPORT_SETTINGS="json+pack"
+	EXPORT_SETTINGS="binary+pack"
 fi
 
 echo "Spine exe: $SPINE_EXE"
@@ -45,52 +45,23 @@ spine_args=""
 spine_args="${spine_args} -u $SPINE_VERSION"
 spine_args="${spine_args} ${@:2}"
 
+pack_file="./packsetting.pack.json"
 while IFS= read -r -d $'\0' file; do
-	echo "Processing $file"
 	setting_file="${file%.*}.export.json"
+
 	if [ ! -f "${setting_file}" ]; then
 		setting_file="${EXPORT_SETTINGS}"
 	fi
 	spine_args=$"${spine_args} -i $file -o `dirname ${EXPORT_PATH}/$file` -e ${setting_file}"
 
+	directory=$(dirname "$file")
+	imgDirectory="${directory}/image"
+
+	if [ ! -d "${imgDirectory}" ]; then
+		imgDirectory="${directory}/images"
+	fi
+
+	spine_args=$"${spine_args} -i $imgDirectory -o `dirname ${EXPORT_PATH}/$file` --pack ${pack_file}"
 done < <(find "${GRAPHICS_PATH}" -iname \*.spine -type f -print0)
-
-
-
-
-
-# Get the current path
-current_path=$(pwd)
-
-# List all directories in the current path
-directories=$(ls -l $current_path | grep "^d" | awk '{print $NF}')
-
-# Display the directories
-echo "Directories in $current_path:"
-echo "$directories"
-
-# Iterate through each directory
-for dir in $directories; do
-    echo "Entering directory: $dir"
-    cd "$current_path/$dir" || exit 1
-
-    # Perform actions inside the directory
-    # List files in the current directory and store them in an array
-    files_in_dir=($(ls))
-
-    echo "Files in $dir:"
-    echo "${files_in_dir[@]}"
-
-    # Print each file in a new line
-    echo "  Files in $dir (on a new line):"
-    for file in "${files_in_dir[@]}"; do
-        echo "    $file"
-    done
-
-    # Return to the original directory
-    cd "$current_path" || exit 1
-done
-
-echo "Processing Done."
 
 "$SPINE_EXE" $spine_args
